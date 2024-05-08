eadcc4fd908dde1292c7f0b760ed39ca6ce48d64
DanielaPeixotoWork <danielapeixoto.work@gmail.com>
2024-05-08 11:50:44 +0100

    Finished exercise 1

--- IntCollections/Program.cs
+++ IntCollections/Program.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections;
 
-namespace IntCollections
+class Program
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        List<int> lista = new List<int>();
+        Stack<int> pilha = new Stack<int>();
+        Queue<int> fila = new Queue<int>();
+        HashSet<int> conjunto = new HashSet<int>();
+
+        int[] valores = { 1, 10, -30, 10, -5 };
+
+        foreach (int valor in valores)
         {
-            Console.WriteLine("Hello, World!");
+            lista.Add(valor);
+            pilha.Push(valor);
+            fila.Enqueue(valor);
+            conjunto.Add(valor);
         }
+
+        Console.Write("List: ");
+        foreach (int valor in lista)
+        {
+            Console.Write(valor + ", ");
+        }
+        Console.WriteLine();
+
+        Console.Write("Stack: ");
+        foreach (int valor in pilha)
+        {
+            Console.Write(valor + ", ");
+        }
+        Console.WriteLine();
+
+        Console.Write("Queue: ");
+        foreach (int valor in fila)
+        {
+            Console.Write(valor + ", ");
+        }
+        Console.WriteLine();
+
+        Console.Write("HashSet: ");
+        foreach (int valor in conjunto)
+        {
+            Console.Write(valor + ", ");
+        }
+        Console.WriteLine();
     }
 }
